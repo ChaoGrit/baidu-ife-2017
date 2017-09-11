47d6fe6d9dc53a3a064d448eec1dabb86c840ba9
chao <=chaoleisure@gmail.com>
2017-09-12 00:15:54 +0800

    $.extend实现

--- jquery-2.0.3/jq.part3.js
+++ jquery-2.0.3/jq.part3.js
@@ -15,48 +15,51 @@ jQuery.extend = jQuery.fn.extend = function() {
 
 	// Handle a deep copy situation
 	if ( typeof target === "boolean" ) {//是否是深拷贝
-		deep = target;
-		target = arguments[1] || {};
+		deep = target;//把值给deep
+		target = arguments[1] || {};//对target重新赋值，直接用了arguments的属性，不需要直接传参过来
 		// skip the boolean and the target
 		i = 2;
 	}
 
 	// Handle case when target is a string or something (possible in deep copy)
 	if ( typeof target !== "object" && !jQuery.isFunction(target) ) {//参数是否正确
-		target = {};
+		target = {};//目标不是对象或不是一个函数的话，就把它变为一个对象
 	}
 
 	// extend jQuery itself if only one argument is passed
-	if ( length === i ) {//拓展插件时只传一个对象
-		target = this;
-		--i;
+	if ( length === i ) {//只传一个对象，判断是不是插件的情况
+		target = this;//只传一个对象时，目标元素就是this-> $ 或 $() 对象
+		--i;//--i可以参与到下面的循环中
 	}
 
 	for ( ; i < length; i++ ) {
 		// Only deal with non-null/undefined values
-		if ( (options = arguments[ i ]) != null ) {
+		if ( (options = arguments[ i ]) != null ) {//后面的对象arguments[]i是否是非non情况
 			// Extend the base object
-			for ( name in options ) {
-				src = target[ name ];
+			for ( name in options ) {//option是后续对象，name是对象的属性名
+				src = target[ name ];//
 				copy = options[ name ];
 
 				// Prevent never-ending loop
-				if ( target === copy ) {
-					continue;
+				if ( target === copy ) {// a={}; $.extend(a,{name:a})这种情况
+					continue;//跳出循环
 				}
 
 				// Recurse if we're merging plain objects or arrays
 				if ( deep && copy && ( jQuery.isPlainObject(copy) || (copyIsArray = jQuery.isArray(copy)) ) ) {
-					if ( copyIsArray ) {
+					if ( copyIsArray ) {//数组的情况
 						copyIsArray = false;
 						clone = src && jQuery.isArray(src) ? src : [];
 
-					} else {
-						clone = src && jQuery.isPlainObject(src) ? src : {};
+					} else {//对象的情况,感觉有点类似模块化的宽放大模式
+						clone = src && jQuery.isPlainObject(src) ? src : {};//src已经存在&&src是否是json？src：{}
+						//var a = {person:{job:'it'}};
+						//var b = {person:{age:'25'}};
+						//$.extend(true,a,b);
 					}
 
 					// Never move original objects, clone them
-					target[ name ] = jQuery.extend( deep, clone, copy );
+					target[ name ] = jQuery.extend( deep, clone, copy );//递归，针对三层以上的对象
 
 				// Don't bring in undefined values
 				} else if ( copy !== undefined ) {
--- jquery-2.0.3/part3-jq继承的方法/part3.simplify.js
+++ jquery-2.0.3/part3-jq继承的方法/part3.simplify.js
@@ -1,6 +1,19 @@
 
 
-jQuery.fn = jQuery.prototype = {
+
+
+jQuery.extend = jQuery.fn.extend= jQuery.prototype.extend = function() {
+	//注：传入的参数统一由arguments获取函数内部参数
+	/*
+	**继承：
+	**JQ中：拷贝继承
+	**JS：类式继承/原型继承-> 通过构造函数 new xx()/{}.prototype=xxx
+	**$.extend() -> this 是 $ -> this.aaa(jQuery上挂载) ->调用 $.aaa();
+	**$.fn.extend() -> this 是 $.fn -> this.aaa(即原型上挂载) ->调用时要先创建对象才能调用得到 $().aaa();
+	**
+	**
+	*/
+
 
 	定义了一些变量
 
