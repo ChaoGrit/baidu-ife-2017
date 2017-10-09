855de1cdb212eab1ce6973dd4d11de0d16978a3b
youno <chaoleisure@gamail.com>
2017-10-09 22:31:42 +0800

    上传图片后展示

--- node-demo/uploadImageWeb/requestHandler.js
+++ node-demo/uploadImageWeb/requestHandler.js
@@ -191,9 +191,9 @@ function upload(response, request) {
     });
 }
 
-function show(response, postData) {
+function show(response) {
     console.log("Request handler 'show' was called.");
-    fs.readFile("/tmp/test.png", "binary", function (error, file) {
+    fs.readFile("./tmp/test.png", "binary", function (error, file) {
         if (error) {
             response.writeHead(500, { "Content-Type": "text/plain" });
             response.write(error + "\n");
